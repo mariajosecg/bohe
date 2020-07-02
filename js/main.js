6561108c44a7412169f860eb9a3151baf3a89c5d
mariajosecg <castrogonzalezmariajose6@gmail.com>
2020-07-02 14:06:26 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.banner').slick({
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+    prevArrow:"<div class='slick-prev'></div>",
+    nextArrow:"<div class='slick-next'></div>"
+  });
+});
