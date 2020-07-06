cc3dfbf026aa92c3b28f508263788afde5029e54
mariajosecg <castrogonzalezmariajose6@gmail.com>
2020-07-05 23:32:14 -0400

    Multiple details

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   $('.banner').slick({
-	  infinite: true,
-	  speed: 500,
-	  fade: true,
-	  cssEase: 'linear',
+	infinite: true,
+	speed: 500,
+	fade: true,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	cssEase: 'linear',
     prevArrow:"<div class='slick-prev'></div>",
     nextArrow:"<div class='slick-next'></div>"
   });
